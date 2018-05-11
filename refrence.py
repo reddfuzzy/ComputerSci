bc3a7dfa4d9ea62a9f5c752cc0cf31cab9f08764
Nick Stein <34309290+reddfuzzy@users.noreply.github.com>
2018-05-11 09:11:35 -0500

    Initial commit

---
+++ refrence.py
@@ -0,0 +1,14 @@
+'''
+import file system
+'''
+import matplotlib.pyplot as plt
+import os.path
+import numpy as np      # “as” lets us use standard abbreviations
+
+'''Read the image data'''
+# Get the directory of this python script
+directory = os.path.dirname(os.path.abspath(__file__))
+# Build an absolute filename from directory + filename
+filename = os.path.join(directory, 'woman.jpg')
+# Read the image data into an array
+img = plt.imread(filename)
